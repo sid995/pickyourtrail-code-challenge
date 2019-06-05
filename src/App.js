8856b4edb923745d4648bed373b1310d8f3ed92f
Siddharth Kundu <sk31395@gmail.com>
2019-06-05 12:39:17 +0530

    created base for sitemap

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Sitemap from './components/Sitemap'
+
+function App() {
+  return (
+    <div className="main-container">
+      <Sitemap />
+    </div>
+  )
+}
+
+export default App
